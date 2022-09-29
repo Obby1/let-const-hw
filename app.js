dfa17fde2389d4c35a99880aeba4bf005bc264cb
Obby <obbydelrosario@gmail.com>
2022-09-29 15:32:04 -0700

    added JS and HTML files for hw

---
+++ app.js
@@ -0,0 +1,9 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+/* Write an ES2015 Version */
+const PI = 3.14;
+
+// var variables can be reassigned and redeclared. let can be redeclared but not reassigned. let has block scope
+// const variables cannot be reassigned or redeclared. const has block scope
+// let can be reassigned by const cannot. let can be declared as nothing ie. let letVar; but cannot do that with const
+// hoisting is when var variables are loaded onto the window when it's compiled
